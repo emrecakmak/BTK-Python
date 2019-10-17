c9457b14f3a9957e164cfe1e7bfce91f84255846
Emre <emre.cakmak@ug.bilkent.edu.tr>
2019-10-17 22:04:59 +0300

    homeworks added.

---
+++ BTK-HW2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 16 20:58:51 2019
+
+@author: Emre
+"""
+#Star app
+
+n=input("Please enter the size of star you want: ")
+
+for i in range(int(n)+1):
+    print(i*"*")
+    
+    
